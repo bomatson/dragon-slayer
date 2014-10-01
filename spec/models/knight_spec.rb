53cdc7082a41bdcf991195f01ae0dc7e83b3c70d
bomatson <bobby@carbonfive.com>
2014-10-01 12:27:08 -0700

    include initial spec for attrs

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -4,15 +4,15 @@ describe Knight do
   subject { FactoryGirl.create(:knight) }
 
   describe 'defaults' do
-    context 'armor' do
-      it 'is set to 500' do
-
+    context 'health' do
+      it 'is set to 100' do
+        expect(subject.health).to eq 100
       end
     end
 
     context 'weapon' do
       it 'is a sword' do
-
+        expect(subject.weapon).to eq 'sword'
       end
     end
   end
